b46a79d9c57700d0ded617702f0b4c41dd6311ae
Linarz11 <linarz@bk.ru>
2021-04-02 18:21:14 +0300

    minus DONE

--- Intro/Fraction/Source.cpp
+++ Intro/Fraction/Source.cpp
@@ -5,6 +5,8 @@ using namespace std;
 //#define MY_WORK
 //#define CONSTRUCTORS_CHECK
 #define tab "\t"
+#define OPERATORS_CHECK
+
 
 class Fraction
 {
@@ -71,6 +73,10 @@ public:
 	int numerator; //Числитель
 	int denominator;//Знаменатель
 public:
+	bool get_minus()const
+	{
+		return minus;
+	}
 	int get_integer()const
 	{
 		return integer;
@@ -89,55 +95,132 @@ public:
 	void set_numerator(int numerator)
 	{
 		this->numerator = numerator;
+
 	}void set_denominator(int denominator)
 	{
 		if (denominator == 0)denominator = 1;
 		this->denominator = denominator;
 	}
-//Constructors
+	void set_minus(bool minus)
+	{
+		this->minus = minus;
+	}
+	Fraction& set_minus_to_number()
+	{
+		if (minus)
+		{
+			integer = -integer;
+			numerator = -numerator;
+			minus = false;
+		}
+	return *this;
+	}
+Fraction& get_minus_from_number()
+	{
+		if (integer < 0)
+		{
+			integer = -integer;
+			minus = true;
+		}
+		if (numerator < 0)
+		{
+			numerator = -numerator;
+			minus = true;
+		}
+		return *this;
+
+	}
+	//Constructors
+
+
 	Fraction()
 	{
+		this->minus = false;
 		this->integer = 0;
-	this->numerator = 0;
-	this->denominator = 1;
-	cout << "Default COnstructor0:\t" << this << endl;
+		this->numerator = 0;
+		this->denominator = 1;
+#ifdef DEBUG
+		cout << "Default COnstructor0:\t" << this << endl;
+
+#endif // DEBUG
 	}
 	Fraction(int integer)
 	{
+		this->minus = false;
 		this->integer = integer;
 		this->numerator = 0;
 		this->denominator = 1;
+		if (integer < 0)
+		{
+			minus = true;
+			this->integer = -integer;
+		}
+#ifdef DEBUG
 		cout << "One ARG Constructor1:\t" << this << endl;
+
+#endif // DEBUG
 	}
 	Fraction(int numerator, int denominator)
 	{
+		this->minus = false;
 		this->integer = 0;
 		this->numerator = numerator;
 		this->set_denominator(denominator);
+		if (numerator < 0)
+		{
+			minus = true;
+			this->numerator = -numerator;
+		}
+
+#ifdef DEBUG
 		cout << "COnstructor2:\t" << this << endl;
+
+#endif // DEBUG
 	}
 
 	Fraction(int integer, int numerator, int denominator)
 	{
+		this->minus = false;
 		this->integer = integer;
-		this-> numerator = numerator;
+		this->numerator = numerator;
 		this->set_denominator(denominator);
+		if (integer < 0)
+		{
+			minus = true;
+			this->integer = -integer;
+		}
+
+		if (numerator < 0)
+		{
+			minus = true;
+			this->numerator = -numerator;
+		}
+#ifdef DEBUG
 		cout << "Constructor3:\t" << this << endl;
+
+#endif // DEBUG
 	}
 	~Fraction()
 	{
+#ifdef DEBUG
 		cout << "Destructor:\t" << this << endl;
+
+#endif // DEBUG
 	}
 
 	//Methods
 	Fraction& to_proper()
 	{
+		/*if(integer>=0) integer += numerator / denominator;
+		else integer -=numerator / denominator;*/
 		integer += numerator / denominator;
+		get_minus_from_number();
 		numerator %= denominator;
 		return *this;
 	}
 	Fraction& to_improper()
 	{
+		get_minus_from_number();
 		numerator += integer * denominator;
 		integer = 0;
 		return *this;
@@ -148,17 +231,20 @@ Fraction operator+(Fraction left, Fraction right)
 {
 	left.to_proper();
 	right.to_proper();
+	left.set_minus_to_number();
+	right.set_minus_to_number();
 	Fraction result;
 	result.set_integer(left.get_integer() + right.get_integer());
 	result.set_numerator(left.get_numerator() * right.get_denominator() + right.get_numerator() * left.get_denominator());
 	result.set_denominator(left.get_denominator() * right.get_denominator());
-	result.to_proper();
+	result.to_proper().get_minus_from_number();
 	return result;
 }
 Fraction operator-(Fraction left, Fraction right)
 {
-	left.to_proper();
-	right.to_proper();
+	left.to_proper().set_minus_to_number();
+	right.to_proper().set_minus_to_number();
+
 	/*Fraction result;
 	result.set_integer(left.get_integer() - right.get_integer());
 	result.set_numerator(left.get_numerator() * right.get_denominator() - right.get_numerator() * left.get_denominator());
@@ -170,43 +256,51 @@ Fraction operator-(Fraction left, Fraction right)
 		left.get_integer() - right.get_integer(),
 		left.get_numerator() * right.get_denominator() - right.get_numerator() * left.get_denominator(),
 		left.get_denominator() * right.get_denominator()
-	).to_proper();
+	).to_proper().get_minus_from_number();
 }
 
 Fraction operator*(Fraction left, Fraction right)
 {
-	left.to_improper();
-	right.to_improper();
+	left.to_improper().set_minus_to_number();
+	right.to_improper().set_minus_to_number();
 	return Fraction
 	(
 		left.get_numerator() * right.get_numerator(),
 		left.get_denominator() * right.get_denominator()
-	).to_proper();
+	).to_proper().get_minus_from_number();
 }
 
 Fraction operator/(Fraction left, Fraction right)
 {
 	left.to_improper();
 	right.to_improper();
-	return Fraction
+	 Fraction result
 	(
 		left.get_numerator() * right.get_denominator(),
 		left.get_denominator() * right.get_numerator()
-	).to_proper();
+	);
+	 result.to_proper();
+	 if (left.get_minus() || right.get_minus())result.set_minus(true);
+	 if (left.get_minus() && right.get_minus())result.set_minus(false);
+	 return result;
 }
 
 
 
 ostream& operator<<(ostream& os, const Fraction& obj)
 {
+	if (obj.get_minus())os << "(-";
 	if (obj.get_integer())os << obj.get_integer();
 	if (obj.get_integer() && obj.get_numerator())os << "(";
 	if (obj.get_numerator())os << obj.get_numerator() << "/" << obj.get_denominator();
 	if (obj.get_integer() && obj.get_numerator())os << ")";
 	if (obj.get_integer() == 0 && obj.get_numerator() == 0)os << 0;
+	if (obj.get_minus())os << ")";
+
 	return os;
 
 }
+
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -217,15 +311,14 @@ void main()
 #ifdef CONSTRUCTORS_CHECK
 	Fraction A;//Default constructor
 	cout << A << endl;
-	Fraction B = 5;//Single-argument constructor
+	Fraction B = -5;//Single-argument constructor
 	cout << B << endl;
-	Fraction C(1, 2);
+	Fraction C(-1, 2);
 	cout << C << endl;
-	Fraction D(2, 3, 4);
+	Fraction D(-2, 3, 4);
 	cout << D << endl;
-	Fraction A(7,2);
-	cout << A<<" = " << A.to_proper()<< endl;
-	cout << A << " = " << A.to_improper() << endl;
+
+
 #endif // CONSTRUCTORS_CHECK
 
 	/*double a = 2.7;
@@ -233,14 +326,18 @@ void main()
 	cout << a * b << endl;*/
 	Fraction A(2, 3, 4);
 	Fraction B(5, 6, 7);
+#ifdef OPERATORS_CHECK
 	cout << A << tab << B << endl;
 	cout << A << " + " << B << " = " << A + B << endl;
-	cout << B << " - " << B<< " = " << B - B << endl;
-	cout << B << " - " << A<< " = " << B - A << endl;
-	cout << A << " - " << B<< " = " << A - B << endl;
+	cout << B << " - " << B << " = " << B - B << endl;
+	cout << B << " - " << A << " = " << B - A << endl;
+	cout << A << " - " << B << " = " << A - B << endl;
 
-	cout <<A<< " * " <<B<< " = " <<  A * B << endl;
-	cout <<A<< " / " <<B<< " = " <<  A / B << endl;
+	cout << A << " * " << B << " = " << A * B << endl;
+	cout << A << " / " << B << " = " << A / B << endl;
+
+#endif // OPERATORS_CHECK
+
 
 
 }
